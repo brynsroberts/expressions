765f7479baeb7a763939d2b2027f346116d720c7
Bryan <bryan.roberts.home@gmail.com>
2021-01-21 20:45:28 -0800

    add content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+let first = "Bryan";
+let last = "Roberts";
+let d = new Date();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {first + " " + last}</p>
+    <p>Copyright {d.getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
